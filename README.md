7551f9dfff4e1b4b274c1310fa0f607f339a3f35
Knut Kirkhorn <knutkirk@hotmail.com>
2017-11-09 01:18:49 +0100

    Added cli module files

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # gcd-cli
+Get the greatest common devisor of two numbers CLI
+
+## Installation
+```
+$ npm install --global @knutkirkhorn/gcd-cli 
+```
+
+## Usage
+```
+$ gcd --help
+
+        Usage
+          $ gcd <number1> <number2>
+
+        Examples
+          $ gcd 12 20
+          $ gcd 13 234 
+```
+
+## Related
+- [@knutkirkhorn/gcd](https://github.com/Knutakir/gcd)
+
+## Licence
+MIT © [Knut Kirkhorn](LICENSE)
---
+++ cli.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+'use strict;'
+const gcd = require('@knutkirkhorn/gcd');
+const meow = require('meow');
+
+const cli = meow(`
+        Usage
+          $ gcd <number1> <number2>
+
+        Examples
+          $ gcd 12 20
+          $ gcd 13 234 
+`);
+
+const number1 = cli.input[0];
+const number2 = cli.input[1];
+
+if (!(number1 && number2)) {
+    console.log("You need to specify both number1 and number2.");
+    process.exit(1);
+}
+
+console.log(gcd(number1, number2));
